448ee1bb675739b7ad9f453b989cc1a687c7eaea
Jan Rutkowski <janek.rut94@gmail.com>
2022-08-06 17:43:54 +0200

    Initial Commit

---
+++ VerticalSwitch/VerticalSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerticalSwitchApp.swift
+//  VerticalSwitch
+//
+//  Created by Jan Rutkowski on 06/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct VerticalSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
